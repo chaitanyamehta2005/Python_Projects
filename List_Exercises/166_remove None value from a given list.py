b5f17a5f5b1c1cd9a48c8cbbd0d88a4e40a80a11
chaitanyamehta2005 <chaitanyamehta2005@gmail.com>
2022-04-16 18:30:59 +0530

    Refractored code

--- List_Exercises/166_remove None value from a given list.py
+++ List_Exercises/166_remove None value from a given list.py
@@ -1,10 +1,13 @@
 """
+Question:
 Original list:
 [12, 0, None, 23, None, -55, 234, 89, None, 0, 6, -12]
 Remove None value from the said list:
 [12, 0, 23, -55, 234, 89, 0, 6, -12]
 """
+# First take a list with some elements as None.
+input_lst1= [12, 0, None, 23, None, -55, 234, 89, None, 0, 6, -12]
 
-lst1= [12, 0, None, 23, None, -55, 234, 89, None, 0, 6, -12]
-output = [lst1[i] for i in range (len(lst1)) if lst1[i] is not None]
-print(output)
+#Now create a list comprehension after removing the None values
+output_lst = [input_lst1[i] for i in range (len(input_lst1)) if input_lst1[i] is not None]
+print("output after removal of None is {0}".format(output_lst))
