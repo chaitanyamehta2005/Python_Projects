3a107cf3c8566417c24ef0b70e8c5ec4e572161b
chaitanyamehta2005 <chaitanyamehta2005@gmail.com>
2022-04-17 17:50:51 +0530

    Added 172.List code

---
+++ List_Exercises/172_remove_last_N_elements_from_list.py
@@ -0,0 +1,21 @@
+"""
+Question:
+Original lists:
+[2, 3, 9, 8, 2, 0, 39, 84, 2, 2, 34, 2, 34, 5, 3, 5]
+Remove the last 3 elements from the said list:
+[2, 3, 9, 8, 2, 0, 39, 84, 2, 2, 34, 2, 34]
+Remove the last 5 elements from the said list:
+[2, 3, 9, 8, 2, 0, 39, 84, 2, 2, 34]
+Remove the last 1 element from the said list:
+[2, 3, 9, 8, 2, 0, 39, 84, 2, 2, 34, 2, 34, 5, 3]
+"""
+#Following function removes last N elements from list
+def remove_N_elem(lst,N):
+    for i in range (0,N):
+        lst.pop()
+    return lst
+
+orig_list= [2, 3, 9, 8, 2, 0, 39, 84, 2, 2, 34, 2, 34, 5, 3, 5]
+
+output_1 = remove_N_elem(orig_list,3)
+print(output_1)
