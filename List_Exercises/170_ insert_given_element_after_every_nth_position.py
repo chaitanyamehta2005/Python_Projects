fefda66fdec3cbbdff41d504f8807d7fa112cd81
chaitanyamehta2005 <chaitanyamehta2005@gmail.com>
2022-04-17 17:19:39 +0530

    Commit for the change in code exercise 170_list

---
+++ List_Exercises/170_ insert_given_element_after_every_nth_position.py
@@ -0,0 +1,26 @@
+"""
+Question:
+Original list:
+[1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+Insert a in the said list after 2 nd element:
+[1, 2, 'a', 3, 4, 'a', 5, 6, 'a', 7, 8, 'a', 9, 0]
+Insert b in the said list after 4 th element:
+[1, 2, 3, 4, 'b', 5, 6, 7, 8, 'b', 9, 0]
+"""
+def insert_elem_pos(lst,elem,pos):
+    output = []
+    for i in range (0,len(lst),pos):
+        output.extend(lst[i:i+pos])
+        output.append(elem)
+    return output
+
+# Take input list
+input_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+
+# Insert a after every 2nd element
+output_list_1= insert_elem_pos(input_list,'a',2)
+print("new list after inserting a at after every 2nd element is : {0}".format(output_list_1))
+
+# Insert b after every 3rd element
+output_list_2= insert_elem_pos(input_list,'b',3)
+print("new list after inserting b at after every 3rd element is : {0}".format(output_list_2))
