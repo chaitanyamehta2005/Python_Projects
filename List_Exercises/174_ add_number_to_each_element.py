02de5f5030ba500f8699e08b5bf0d9ee987148fb
chaitanyamehta2005 <chaitanyamehta2005@gmail.com>
2022-04-19 08:27:49 +0530

    Added 174_list file

---
+++ List_Exercises/174_ add_number_to_each_element.py
@@ -0,0 +1,22 @@
+"""
+Question:
+Original lists:
+[3, 8, 9, 4, 5, 0, 5, 0, 3]
+Add 3 to each element in the said list:
+[6, 11, 12, 7, 8, 3, 8, 3, 6]
+Original lists:
+[3.2, 8, 9.9, 4.2, 5, 0.1, 5, 3.11, 0]
+Add 0.51 to each element in the said list:
+[3.71, 8.51, 10.41, 4.71, 5.51, 0.61, 5.51, 3.62, 0.51]
+"""
+
+def add_number(lst,num):
+    output=[i+num for i in lst]
+    return output
+
+
+origin_lst=[3.2, 8, 9.9, 4.2, 5, 0.1, 5, 3.11, 0]
+print(add_number(origin_lst,0.51))
+
+origin_lst=[3.2, 8, 9.9, 4.2, 5, 0.1, 5, 3.11, 0]
+print(add_number(origin_lst,0.51))
