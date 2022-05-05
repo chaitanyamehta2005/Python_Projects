bba4788b58f0e81e7e1e362a2396ec79f20f137d
chaitanyamehta2005 <chaitanyamehta2005@gmail.com>
2022-05-05 09:13:13 +0530

    refractored code with comments.

--- Networking_and_CyberSecurity/002_Port_Scanner_using_socket.py
+++ Networking_and_CyberSecurity/002_Port_Scanner_using_socket.py
@@ -1,8 +1,8 @@
 import socket
 
-# creating the socket object
 host = input("Enter the host ip address or domainname: ")
 for port in range(1,65535):
+    # creating the socket object
     socketObject= socket.socket()
 
     result =socketObject.connect_ex((host,port))
@@ -11,5 +11,5 @@ for port in range(1,65535):
         print("Port {0} is open".format(port))
     else:
         print("Port {0} is NOT open".format(port))
-
+    # destructing the socket object
     socketObject.close()
