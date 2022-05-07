37d878e4b32eebc3f16d14016a20b20c90885c78
chaitanyamehta2005 <chaitanyamehta2005@gmail.com>
2022-05-07 06:06:18 +0530

    Refractored code

--- List_Exercises/210_sum_of_nonzero_group.py
+++ List_Exercises/210_sum_of_nonzero_group.py
@@ -5,27 +5,23 @@ Original list:
 Compute the sum of non-zero groups (separated by zeros) of the said list of numbers: [15, 38, 15, 27]
 """
 
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
 def test(lst):
-    sum_lst =[]
-    previous =0
+    sum_list = []
+    sum = 0
 
     for elem in lst:
-        current = elem
-        if previous !=0 and current !=0:
-            sum = sum+current
-        if previous ==0 and current !=0:
+        if int(elem) != 0:
+            sum = sum + int(elem)
+        else:
+            if sum != 0:
+                sum_list.append(sum)
             sum = 0
-            sum = sum+current
-        if previous!=0 and current ==0:
-            sum = sum + current
-            sum_lst.append(sum)
-        previous =current
-    return sum_lst
+    sum_list.append(sum)
+    return sum_list
 
-
-
-
-orig_lst = [3, 4, 6, 2, 0, 0, 0, 0, 0, 0, 6, 7, 6, 9, 10, 0, 0, 0, 0, 0, 7, 4, 4, 0, 0, 0, 0, 0, 0, 5, 3, 2, 9, 7, 1, 0, 0, 0]
+orig_lst = [3, 4, 6, 2, 0, 0, 0, 0, 0, 0, 6, 7, 6, 9, 10, 0, 0, 0, 0, 0, 7, 4, 4, 0, 0, 0, 0, 0, 0, 5, 3, 2, 9, 7, 1]
 
 print(test(orig_lst))
 
