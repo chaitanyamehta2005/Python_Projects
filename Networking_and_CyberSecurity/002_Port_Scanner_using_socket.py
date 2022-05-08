1375e32dd1b39cf7400411bffd14af5bdccd25e7
chaitanyamehta2005 <chaitanyamehta2005@gmail.com>
2022-05-08 23:38:05 +0530

    Refactored using threading

--- Networking_and_CyberSecurity/002_Port_Scanner_using_socket.py
+++ Networking_and_CyberSecurity/002_Port_Scanner_using_socket.py
@@ -1,15 +1,24 @@
 import socket
+import threading
+import time
+import socket
 
 host = input("Enter the host ip address or domainname: ")
-for port in range(1,65535):
-    # creating the socket object
-    socketObject= socket.socket()
 
-    result =socketObject.connect_ex((host,port))
+def port_scanner(host,port):
+    socketObject = socket.socket()
+    try:
+        connection =socketObject.connect((host, port))
+        print('port {0} is open'.format(port))
+        connection.close()
+    except:
+        #print('port {0} is closed'.format(port))
+        pass
 
-    if result ==0:
-        print("Port {0} is open".format(port))
-    else:
-        print("Port {0} is NOT open".format(port))
-    # destructing the socket object
-    socketObject.close()
+threads = []
+for x in range(1,65535):
+    t = threading.Thread(target = port_scanner, args=(host,x))
+    threads.append(t)
+    t.start()
+
+
