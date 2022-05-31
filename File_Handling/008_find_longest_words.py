4993bcdb1dc37579023cb58669eb75b186cfeb9c
chaitanyamehta2005 <chaitanyamehta2005@gmail.com>
2022-05-31 09:52:28 +0530

    Initial Commit 008_FileIO

---
+++ File_Handling/008_find_longest_words.py
@@ -0,0 +1,10 @@
+def find_longest_word(filename):
+    with open(filename, 'r') as infile:
+              words = infile.read().split()
+    max_len = len(max(words, key=len))
+    return [i for i in words if len(i) == max_len]
+
+
+filename = r"C:\Users\Chaitanya\Documents\Hello.txt"
+output = find_longest_word(filename)
+print(output)
