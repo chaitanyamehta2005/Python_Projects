50c897d8f7b8a6a3857c1e2767f8be6388bc09e7
chaitanyamehta2005 <chaitanyamehta2005@gmail.com>
2022-06-01 18:30:49 +0530

    Refactored code

---
+++ File_Handling/009_count_number_lines_in_text_file.py
@@ -0,0 +1,14 @@
+def count_lines(filename):
+    count = 0
+    with open(filename,'r') as fileObj:
+        while True:
+            data = fileObj.readline()
+            if data =='':
+                break
+            else:
+                count+=1
+    return count
+
+filename = r"C:\Users\Chaitanya\Documents\Hello.txt"
+total_lines = count_lines(filename)
+print("There are total {0} line(s) in the file".format(total_lines))
